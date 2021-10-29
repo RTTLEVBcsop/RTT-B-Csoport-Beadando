6b44079f8317aa6515820941f4ffb9f51fd3166e
szabocs02 <barcikai82@gmail.com>
2021-10-29 16:05:38 +0200

    frontend
    alap oldal

---
+++ RTT/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Programtervező cég</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <header class="header">
+        <nav class="navbar navbar-style">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                <a href=""><img class="logo" src="team-g8e083dc6d_1920.jpg"></a>
+                </div>
+                <div class="collapse navbar-collapse" id="micon">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Features</a></li>
+                    <li><a href="">Team</a></li>
+                    <li><a href="">Gallery</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>            
+                </div>
+                </div>
+                </nav>
+
+<div class="container">
+<div class="row">
+
+    <div class="col-sm-6 banner-info">
+        <h1>XY vállalat</h1>
+        <p class="big-text">Programtervező informatikusokat keresünk!!!</p>
+        <p>Ha érdekel, akkor töltsd fel az önéletrajzod.</p>
+        <a class="btn btn-first" href="#">regisztráció</a>
+        <a class="btn btn-second" href="#">belépés</a>
+    </div>
+
+    <div class="col-sm-6 banner-image" >
+    <img src="meeting-gf53a7d63f_6401.jpg" class="img.responsive">
+    
+    </div>
+</div>
+</div>
+
+
+    </header>
+    
+</body>
+</html>
