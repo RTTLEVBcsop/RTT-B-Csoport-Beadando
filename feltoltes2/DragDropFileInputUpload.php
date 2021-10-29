0ee5e216afd48cdbacdfc44ec43f59182c1416b7
szabocs02 <barcikai82@gmail.com>
2021-10-29 16:12:17 +0200

    feltoltes

---
+++ feltoltes2/DragDropFileInputUpload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="Drag-Drop-File-Input-Upload.css" />
+    <link rel="stylesheet" href="style4.css" />
+</head>
+<body>
+    <div class="files color form-group mb-3">
+        <input type="file" multiple name="files">
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
