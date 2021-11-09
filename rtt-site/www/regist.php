aaa95df5a4034b99cb0e535127e7e743047808e9
szabocs02 <92058329+szabocs02@users.noreply.github.com>
2021-11-09 05:06:12 +0100

    Update regist.php

--- rtt-site/www/regist.php
+++ rtt-site/www/regist.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/var/www/rtt/css/style2.css">
+    <link rel="stylesheet" type="text/css" href="/RTT-B-Csoport-Beadando/rtt-site/css/style2.css">
 </head>
 <body>
 
